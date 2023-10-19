41314192cf18f43e9b795de5ac0b9fcaa618e445
alunturner <alun.turner@googlemail.com>
2023-10-19 11:30:34 +0100

    add styling and duplication of link text

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -3,20 +3,40 @@ import styled from 'styled-components';
 
 import { WEIGHTS } from "../../constants";
 
-const NavLink = ({children, ...delegated}) => {
-    return <Wrapper {...delegated}>{children}</Wrapper>;
+const NavLink = ({ children, ...delegated }) => {
+    return <Wrapper {...delegated}>
+        <MainText>{children}</MainText>
+        <HoverText>{children}</HoverText>
+    </Wrapper>;
 }
 
 const Wrapper = styled.a`
+    position: relative;
+    display: block;
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
   color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
+    overflow: hidden;
 
   &:first-of-type {
     color: var(--color-secondary);
   }
+`;
+
+const Text = styled.span`
+    display: block
+`
+const MainText = styled(Text)`
+    
+`;
+const HoverText = styled(Text)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 `;
 
 export default NavLink;
