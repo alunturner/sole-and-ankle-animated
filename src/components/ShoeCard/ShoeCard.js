2f6e5bae95e22d047ffa7c13150c2110b2101771
alunturner <alun.turner@googlemail.com>
2023-10-19 11:04:50 +0100

    prevent inadvertant truncation of flags

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -6,66 +6,66 @@ import { formatPrice, pluralize, isNewShoe } from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
+    slug,
+    name,
+    imageSrc,
+    price,
+    salePrice,
+    releaseDate,
+    numOfColors,
 }) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
-    ? 'on-sale'
-    : isNewShoe(releaseDate)
-      ? 'new-release'
-      : 'default'
+    // There are 3 variants possible, based on the props:
+    //   - new-release
+    //   - on-sale
+    //   - default
+    //
+    // Any shoe released in the last month will be considered
+    // `new-release`. Any shoe with a `salePrice` will be
+    // on-sale. In theory, it is possible for a shoe to be
+    // both on-sale and new-release, but in this case, `on-sale`
+    // will triumph and be the variant used.
+    // prettier-ignore
+    const variant = typeof salePrice === 'number'
+        ? 'on-sale'
+        : isNewShoe(releaseDate)
+            ? 'new-release'
+            : 'default'
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
-          {variant === 'new-release' && (
-            <NewFlag>Just released!</NewFlag>
-          )}
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price
-            style={{
-              '--color':
-                variant === 'on-sale'
-                  ? 'var(--color-gray-700)'
-                  : undefined,
-              '--text-decoration':
-                variant === 'on-sale' ? 'line-through' : undefined,
-            }}
-          >
-            {formatPrice(price)}
-          </Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-          {variant === 'on-sale' ? (
-            <SalePrice>{formatPrice(salePrice)}</SalePrice>
-          ) : undefined}
-        </Row>
-      </Wrapper>
-    </Link>
-  );
+    return (
+        <Link href={`/shoe/${slug}`}>
+            <Wrapper>
+                <ImageWrapper>
+                    <Image alt="" src={imageSrc} />
+                </ImageWrapper>
+                {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+                {variant === 'new-release' && (
+                    <NewFlag>Just released!</NewFlag>
+                )}
+                <Spacer size={12} />
+                <Row>
+                    <Name>{name}</Name>
+                    <Price
+                        style={{
+                            '--color':
+                                variant === 'on-sale'
+                                    ? 'var(--color-gray-700)'
+                                    : undefined,
+                            '--text-decoration':
+                                variant === 'on-sale' ? 'line-through' : undefined,
+                        }}
+                    >
+                        {formatPrice(price)}
+                    </Price>
+                </Row>
+                <Row>
+                    <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+                    {variant === 'on-sale' ? (
+                        <SalePrice>{formatPrice(salePrice)}</SalePrice>
+                    ) : undefined}
+                </Row>
+            </Wrapper>
+        </Link>
+    );
 };
 const ImageWrapper = styled.div`
 position: relative;
@@ -89,7 +89,9 @@ const Link = styled.a`
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+    position: relative;
+`;
 
 
 const Row = styled.div`
