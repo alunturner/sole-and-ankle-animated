500a6c58efe03cec2ce0194c1fe799f69f264fe1
alunturner <alun.turner@googlemail.com>
2023-10-19 11:47:19 +0100

    add mobile menu animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
 import { QUERIES, WEIGHTS } from '../../constants';
@@ -10,31 +10,39 @@ import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  return (
-    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <Content aria-label="Menu">
-        <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
-          <VisuallyHidden>Dismiss menu</VisuallyHidden>
-        </CloseButton>
-        <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
-        </Footer>
-      </Content>
-    </Overlay>
-  );
+    return (
+        <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+            <Content aria-label="Menu">
+                <CloseButton onClick={onDismiss}>
+                    <Icon id="close" />
+                    <VisuallyHidden>Dismiss menu</VisuallyHidden>
+                </CloseButton>
+                <Filler />
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <Footer>
+                    <SubLink href="/terms">Terms and Conditions</SubLink>
+                    <SubLink href="/privacy">Privacy Policy</SubLink>
+                    <SubLink href="/contact">Contact Us</SubLink>
+                </Footer>
+            </Content>
+        </Overlay>
+    );
 };
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`
 
 const Overlay = styled(DialogOverlay)`
   position: fixed;
@@ -45,6 +53,7 @@ const Overlay = styled(DialogOverlay)`
   background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
+    animation: ${fadeIn} 750ms;
 `;
 
 const Content = styled(DialogContent)`
